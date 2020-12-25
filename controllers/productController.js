1a7d7eed6d13df8191aa66a2472f8eb60ddab57c
richCode <chineduogada@gmail.com>
2020-12-25 22:16:01 +0100

    feature(catchAsync):add util function
    Add a `util` function, to catch unexpected async errors. This eliminate the
    redundent `trycatch` block.
    
    Use to wrap asynchronous controller function majorly.
    
    [Delivers #2]

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,51 +1,48 @@
 const Joi = require("joi");
 const Product = require("../models/productModel");
+const catchAsync = require("../utils/catchAsync");
+// const AppError = require("../utils/AppError");
 
-exports.getAllProducts = async (req, res, next) => {
+exports.getAllProducts = catchAsync(async (req, res, next) => {
 	res.send("all products :)");
-};
+});
 
-exports.createProduct = async (req, res, next) => {
-	try {
-		const schema = Joi.object({
-			title: Joi.string().min(5).max(25).required(),
+exports.createProduct = catchAsync(async (req, res, next) => {
+	const schema = Joi.object({
+		title: Joi.string().min(5).max(25).required(),
 
-			price: Joi.number().min(10).required(),
-			description: Joi.string().min(25).max(500).required(),
-			category: Joi.string()
-				.valid("computing", "phones-and-tablets")
-				.required(),
-			discountPercentage: Joi.number(),
-			shippedFromAbroad: Joi.boolean(),
-			dealOffer: Joi.string(),
-			ItemsInBox: Joi.array().items(
-				Joi.object({
-					name: Joi.string().required(),
-					value: Joi.string().required(),
-				})
-			),
-			ratingsQuantity: Joi.number(),
-			ratingsAverage: Joi.number().min(1).max(5),
-			imageCover: Joi.string().required(),
-			images: Joi.string(),
-		});
-		const { error } = schema.validate(req.body);
+		price: Joi.number().min(10).required(),
+		description: Joi.string().min(25).max(500).required(),
+		category: Joi.string()
+			.valid("computing", "phones-and-tablets")
+			.required(),
+		discountPercentage: Joi.number(),
+		shippedFromAbroad: Joi.boolean(),
+		dealOffer: Joi.string(),
+		ItemsInBox: Joi.array().items(
+			Joi.object({
+				name: Joi.string().required(),
+				value: Joi.string().required(),
+			})
+		),
+		ratingsQuantity: Joi.number(),
+		ratingsAverage: Joi.number().min(1).max(5),
+		imageCover: Joi.string().required(),
+		images: Joi.string(),
+	});
+	const { error } = schema.validate(req.body);
 
-		if (error) {
-			return res.send(error.details[0].message);
-		}
+	// if (error) {
+	// 	return next(new AppError(error.details[0].message, 400));
+	// }
 
-		const product = await Product.create(req.body);
+	const product = await Product.create(req.body);
 
-		res.status(201).json({
-			status: "success",
-			data: {
-				product,
-			},
-		});
-	} catch (error) {
-		console.log(error);
-		res.send("fail");
-	}
-};
+	res.status(201).json({
+		status: "success",
+		data: {
+			product,
+		},
+	});
+});
 
