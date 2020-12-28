8bc3fb8622f3036a212f8dc4039a6835bcf9c17b
richCode <chineduogada@gmail.com>
2020-12-28 02:29:45 +0100

    feature(User):add model
    - Define User schema.
    - Add `signup` authentication controller, with no implementation
    
    [Start #3 #4]

---
+++ controllers/authController.js
@@ -0,0 +1,5 @@
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+
+exports.signUp = catchAsync((req, res, next) => {});
+
---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const schema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "`name` is required"],
+		minlength: [3, "`name` can be '3' or more characters"],
+		maxlength: [25, "`name` can be '25' or less characters"],
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: [true, "`password` is required"],
+		minlength: [8, "`name` can be '8' or more characters"],
+		select: false,
+	},
+	email: {
+		type: String,
+		validate: [validator.isEmail, "`email` must be valid"],
+		required: [true, "`email` is required"],
+		unique: true,
+		lowercase: true,
+		select: false,
+	},
+	photo: String,
+	bio: {
+		type: String,
+		trim: true,
+		required: [true, "`name` is required"],
+		minlength: [10, "`name` can be '10' or more characters"],
+		maxlength: [20, "`name` can be '20' or less characters"],
+	},
+	role: {
+		type: String,
+		enum: {
+			values: ["buyer", "seller", "admin"],
+			validate: "`category` can be either 'buyer', 'seller' or 'admin'",
+		},
+		default: "buyer",
+	},
+	createAt: {
+		type: Date,
+		select: false,
+		default: Date.now,
+	},
+});
+
+const User = mongoose.model("User", schema);
+
+module.exports = User;
+
