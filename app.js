22d612e0fa8c9892ee75603fa76abf178fb4729f
RichCode <chineduogada@gmail.com>
2021-01-09 04:40:52 -0800

    bug(Redis):change `second-key` implementation
    Add the query `option` to the `second-key`, for constitentcy.
    
    [Fixes #10]

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
  */
 
 const express = require("express");
+const cors = require("cors");
 const productRouter = require("./routes/productRouter");
 const userRouter = require("./routes/userRouter");
 const errorController = require("./controllers/errorController");
@@ -15,20 +16,21 @@ require("./services/cache");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/users", userRouter);
 
 app.use("*", (req, _res, next) => {
-	next(
-		new AppError(
-			`can't find ${req.originalUrl}, with the method:${req.method}, on this server!`
-		)
-	);
+  next(
+    new AppError(
+      `can't find ${req.originalUrl}, with the method:${req.method}, on this server!`
+    )
+  );
 });
 
 app.use(errorController);
 
 module.exports = app;
-
--- utils/APIFeatures.js
+++ utils/APIFeatures.js
@@ -1,64 +1,63 @@
 class APIFeatures {
-	constructor(mongooseQuery, reqQuery) {
-		this.Query = mongooseQuery;
-		this.queryString = reqQuery;
-	}
+  constructor(mongooseQuery, reqQuery) {
+    this.Query = mongooseQuery;
+    this.queryString = reqQuery;
+  }
 
-	filter = () => {
-		let queryObject = { ...this.queryString };
-		// Delete special params fields
-		const excludedFields = ["fields", "page", "limit", "sort"];
-		excludedFields.forEach((field) =>
-			Reflect.deleteProperty(queryObject, field)
-		);
+  filter = () => {
+    let queryObject = { ...this.queryString };
+    // Delete special params fields
+    const excludedFields = ["fields", "page", "limit", "sort"];
+    excludedFields.forEach((field) =>
+      Reflect.deleteProperty(queryObject, field)
+    );
 
-		let queryString = JSON.stringify(queryObject);
-		const replacePat = /(gt|lt|gte|lte)/;
-		const replaceFn = (match) => `$${match}`;
-		queryString = queryString.replace(replacePat, replaceFn);
+    let queryString = JSON.stringify(queryObject);
+    const replacePat = /(gt|lt|gte|lte)/;
+    const replaceFn = (match) => `$${match}`;
+    queryString = queryString.replace(replacePat, replaceFn);
 
-		queryObject = JSON.parse(queryString);
-		this.Query.find(queryObject);
+    queryObject = JSON.parse(queryString);
+    this.Query.find(queryObject);
 
-		return this;
-	};
+    return this;
+  };
 
-	sort = () => {
-		if (this.queryString.sort) {
-			let sortBy = this.queryString.sort;
-			sortBy = sortBy.replace(/,/g, " ");
+  sort = () => {
+    if (this.queryString.sort) {
+      let sortBy = this.queryString.sort;
+      sortBy = sortBy.replace(/,/g, " ");
 
-			this.Query = this.Query.sort(sortBy);
-		} else {
-			this.Query = this.Query.sort("-createdAt");
-		}
+      this.Query.sort(sortBy);
+    } else {
+      this.Query.sort("-createdAt");
+    }
 
-		return this;
-	};
+    return this;
+  };
 
-	project = () => {
-		if (this.queryString.fields) {
-			let fields = this.queryString.fields;
-			fields = fields.replace(/,/g, " ");
+  project = () => {
+    if (this.queryString.fields) {
+      let fields = this.queryString.fields;
+      fields = fields.replace(/,/g, " ");
 
-			this.Query = this.Query.select(fields);
-		} else {
-			this.Query = this.Query.select("-__v");
-		}
+      this.Query.select(fields);
+    } else {
+      this.Query.select("-__v");
+    }
 
-		return this;
-	};
+    return this;
+  };
 
-	paginate = () => {
-		const page = this.queryString.page * 1 || 1;
-		const limit = this.queryString.limit * 1 || 100;
-		const numberOfDocsToSkip = (page - 1) * limit;
+  paginate = () => {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const numberOfDocsToSkip = (page - 1) * limit;
 
-		this.Query = this.Query.limit(limit).skip(numberOfDocsToSkip);
+    this.Query.limit(limit).skip(numberOfDocsToSkip);
 
-		return this;
-	};
+    return this;
+  };
 }
 
 module.exports = APIFeatures;
-
