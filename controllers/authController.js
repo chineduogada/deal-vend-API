74bc29b2efaca0bf999aaa38ca07fc3709415c54
RichCode <chineduogada@gmail.com>
2021-07-23 14:32:10 +0100

    ft(auth): add token as cookie to auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,16 @@ const signJWT = async ({ user, payload }) => {
     expiresIn: process.env.JWT_EXPIRES,
   });
 
-  return token;
+  const cookieOptions = {
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    cookieOptions.secure = true;
+  }
+
+  return { token, cookieOptions };
 };
 
 exports.inputEmailAddress = catchAsync(async (req, _res, next) => {
@@ -70,7 +79,9 @@ exports.signUp = catchAsync(async (req, res, next) => {
     email: user.email,
   };
 
-  const token = await signJWT({ payload: user });
+  const { token, cookieOptions } = await signJWT({ payload: user });
+
+  res.cookie("token", token, cookieOptions);
 
   res.status(201).json({
     status: "success",
@@ -84,16 +95,6 @@ exports.signUp = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-  const schema = Joi.object({
-    password: Joi.string().min(8).required(),
-    email: Joi.string().email().required(),
-  });
-
-  const error = validateInput(req.body, schema);
-  if (error) {
-    return next(new AppError(error.details[0].message, 400));
-  }
-
   const existingUser = await User.findOne({ email: req.body.email }).select(
     "+password +email"
   );
@@ -105,7 +106,7 @@ exports.login = catchAsync(async (req, res, next) => {
       existingUser.password
     ))
   ) {
-    return next(new AppError("invalid `email` or `password`"));
+    return next(new AppError("invalid `email` or `password`", 400));
   }
 
   if (existingUser.passwordResetToken) {
@@ -114,7 +115,9 @@ exports.login = catchAsync(async (req, res, next) => {
     await existingUser.save();
   }
 
-  const token = await signJWT({ user: existingUser });
+  const { token, cookieOptions } = await signJWT({ user: existingUser });
+
+  res.cookie("token", token, cookieOptions);
 
   res.status(200).json({
     status: "success",
@@ -125,9 +128,12 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async (req, _res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+  if (res.cookies.token) {
+    token = res.cookies.token;
+  } else if (
+    (req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")) ||
+    res.cookies.token
   ) {
     token = req.headers.authorization.replace("Bearer ", "");
   }
@@ -205,7 +211,9 @@ exports.changeMyPassword = catchAsync(async (req, res, next) => {
   await existingUser.save();
 
   // Login the user in
-  const token = await signJWT({ user: req.user });
+  const { token, cookieOptions } = await signJWT({ user: req.user });
+
+  res.cookie("token", token, cookieOptions);
 
   res.status(200).json({
     status: "success",
@@ -278,7 +286,10 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   await existingUser.save();
 
-  const token = await signJWT({ user: existingUser });
+  const { token, cookieOptions } = await signJWT({ user: existingUser });
+
+  res.cookie("token", token, cookieOptions);
+
   res.status(200).json({
     status: "success",
     token,
