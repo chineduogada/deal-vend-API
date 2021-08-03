544d922b53b478584f24f491916b36f203b3cac6
RichCode <chineduogada@gmail.com>
2021-08-03 23:07:43 +0100

    bg(auth): remove SameSite="lax"

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,6 +25,7 @@ const signJWT = async ({ user, payload }) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: "none",
   };
 
   if (process.env.NODE_ENV === "production") {
